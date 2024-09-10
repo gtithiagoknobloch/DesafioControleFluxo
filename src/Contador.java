74d1b1f51a857cef1db0a5aa77ff2c7401c6edbe
Thiago Knobloch dos Santos <thiago@knobloch.com.br>
2024-09-09 22:05:22 -0300

    Criando as variáveis

--- src/Contador.java
+++ src/Contador.java
@@ -5,9 +5,18 @@ public class Contador {
         Scanner terminal = new Scanner(System.in);
 
         
-        System.out.println("Olá, vamos Contar!");
+        System.out.println("Olá, vamos verificar quantas interações serão feitas a partir dos dados informados!");
         System.out.println("Informe o parâmetro 1, neste caso um número inteiro.");
+        int numero1 = terminal.nextInt();
+        System.out.println("Muito bem!");
+        System.out.println("Agora, insira o parâmetro 2, lembre-se, um número inteiro");
+        int numero2 = terminal.nextInt();
 
+        try {
+            
+        } catch (Exception e) {
+            
+        }
         
        
     }
