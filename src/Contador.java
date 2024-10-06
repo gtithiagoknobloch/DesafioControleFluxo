60b3bbe403b1ca71ed695f025133562ed9a3cba8
Thiago Knobloch dos Santos <thiago@knobloch.com.br>
2024-10-05 21:48:02 -0300

    Atualizando script

--- src/Contador.java
+++ src/Contador.java
@@ -2,19 +2,14 @@ import java.util.Scanner;
 
 public class Contador {
     public static void main(String[] args) throws Exception {
-        Scanner terminal = new Scanner(System.in);
-
-        
-        System.out.println("Olá, vamos verificar quantas interações serão feitas a partir dos dados informados!");
-        System.out.println("Informe o parâmetro 1, neste caso um número inteiro.");
-        int numero1 = terminal.nextInt();
-        System.out.println("Muito bem!");
-        System.out.println("Agora, insira o parâmetro 2, lembre-se, um número inteiro");
-        int numero2 = terminal.nextInt();
-
-        try {
-            
-        } catch (Exception e) {
+        try (Scanner terminal = new Scanner(System.in)) {
+            System.out.println("Olá, vamos verificar quantas interações serão feitas a partir dos dados informados!");
+            System.out.println("Informe o parâmetro 1, neste caso um número inteiro.");
+            int numero1 = terminal.nextInt();
+            System.out.println("Muito bem!");
+            System.out.println("Agora, insira o parâmetro 2, lembre-se, um número inteiro");
+            int numero2 = terminal.nextInt();
+        }catch (Exception e) {
             
         }
         
